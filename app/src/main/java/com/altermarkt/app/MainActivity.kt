c38d19db0020ca293a36cd962f0d128a0237d7d3
Latifa270 <latifajauharatul4@gmail.com>
2026-05-26 11:28:50 +0700

    edit MainActivity

--- app/src/main/java/com/altermarkt/app/MainActivity.kt
+++ app/src/main/java/com/altermarkt/app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.altermarkt.app.data.local.AppDatabase
+import com.altermarkt.app.data.repository.AuthRepository
 import com.altermarkt.app.ui.AlterMarketNavHost
 import com.altermarkt.app.ui.theme.AlterMarktTheme
 
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         val db = AppDatabase.getInstance(applicationContext)
+        val authRepository = AuthRepository()
 
         setContent {
             AlterMarktTheme {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
 
                 AlterMarketNavHost(
                     navController = navController,
-                    isLoggedIn    = false,
+                    isLoggedIn    = authRepository.isLoggedIn(),
                     db = db
                 )
             }
